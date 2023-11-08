a5e81713adaa7acec26a5454d0b52993bdca0e43
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-08 22:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let text;
+if (confirm("Do you want to proceed?") == true) {
+  text = "You clicked OK. Proceeding...";
+} else {
+  text = "You clicked Cancel. Exiting...";
+}
+window.alert(text);
